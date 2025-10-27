bca665423b539ac511350d6c47d4ba6b3b1c1c5b
thanhha-haha <lethanhha17012@gmail.com>
2025-10-27 14:21:43 +0700

    comment for user

--- securing-web/src/main/java/com/example/securing_web/UserRepository.java
+++ securing-web/src/main/java/com/example/securing_web/UserRepository.java
@@ -6,3 +6,4 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
+// Đây là một giao diện kho lưu trữ người dùng sử dụng Spring Data JPA để truy xuất dữ liệu người dùng từ cơ sở dữ liệu.
